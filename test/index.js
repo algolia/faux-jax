8a162e67708266c9a53b31956c7677c327168639
vvo <vincent@zeroload.net>
2015-03-04 16:09:49 +0100

    make sinon.js work on IE8

--- test/index.js
+++ test/index.js
@@ -3,13 +3,27 @@
 // we use domready@0.3.0 specifically to be compatible
 // with ie6/7/8
 var domready = require('domready');
-var test = require('tape');
+
+// make globals writable to IE<=8,
+// so that we can use sinon.useFakeTimers();
+// this should be done by sinon.js but not in npm/commonJS world
+// see https://github.com/cjohansen/Sinon.JS/pull/600#issuecomment-76154721
+require('writable-window-method')([
+  'setTimeout',
+  'clearTimeout',
+  'setImmediate',
+  'clearImmediate',
+  'setInterval',
+  'clearInterval',
+  'Date'
+]);
 
 var fauxJax = require('../');
 
 domready(run);
 
 function run() {
+  var test = require('tape');
   var support = require('../lib/support');
 
   var bulkRequire = require('bulk-require');
