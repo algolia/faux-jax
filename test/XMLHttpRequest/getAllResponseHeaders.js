c2188cf164a92dd24caa977d10d381d69a10102b
vvo <vincent@zeroload.net>
2015-03-07 10:39:39 +0100

    refactor xhr.getResponseHeaders()

--- test/XMLHttpRequest/getAllResponseHeaders.js
+++ test/XMLHttpRequest/getAllResponseHeaders.js
@@ -2,24 +2,6 @@ var test = require('tape');
 
 var XMLHttpRequest = require('../../lib/XMLHttpRequest/');
 
-function parseHeaders(headers) {
-  var parsedHeaders = {};
-  var pairs = headers.trim().split('\n');
-  var i;
-  var split;
-  var key;
-  var value;
-
-  for (i = 0; i < pairs.length; i++) {
-    split = pairs[i].split(':');
-    key = split.shift().trim();
-    value = split.join(':').trim();
-    parsedHeaders[key] = value;
-  }
-
-  return parsedHeaders;
-}
-
 test('xhr.getAllResponseHeaders() sends empty string when no headers', function(t) {
   var xhr = new XMLHttpRequest();
   t.equal('', xhr.getAllResponseHeaders(), 'we get an empty string');
@@ -27,11 +9,19 @@ test('xhr.getAllResponseHeaders() sends empty string when no headers', function(
 });
 
 test('xhr.getAllResponseHeaders() sends all response headers when present', function(t) {
-  var headers = {'how': 'dy'};
+  var headers = {
+    'how': 'dy',
+    'hai': 'oh'
+  };
+
   var xhr = new XMLHttpRequest();
   xhr.open('GET', '/');
   xhr.send();
   xhr.respond(200, headers);
-  t.deepEqual(parseHeaders(xhr.getAllResponseHeaders()), headers, 'we get all the headers');
+  t.equal(
+    xhr.getAllResponseHeaders(),
+    'how: dy\r\nhai: oh\r\n',
+    'We get all the response headers in a string, formatted accordingly'
+  );
   t.end();
 });
