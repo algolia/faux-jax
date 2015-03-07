f31e714f890e5b6eaba3560f9d148e9b4fad1b33
Terin Stock <terinjokes@gmail.com>
2015-03-06 16:26:28 -0800

    Make xhr.getAllResponseHeaders match the specification
    > The getAllResponseHeaders() method must return response's header list,
    > in list order, as a single byte sequence with each header separated by
    > a 0x0D 0x0A byte pair, and each name and value of a header separated
    > by a 0x3A 0x20 byte pair.

--- test/XMLHttpRequest/getAllResponseHeaders.js
+++ test/XMLHttpRequest/getAllResponseHeaders.js
@@ -2,6 +2,24 @@ var test = require('tape');
 
 var XMLHttpRequest = require('../../lib/XMLHttpRequest/');
 
+function parseHeaders(headers) {
+  var parsedHeaders = {};
+  var pairs = headers.trim().split('\n');
+  var i;
+  var split;
+  var key;
+  var value;
+
+  for (i = 0; i < pairs.length; i++) {
+    split = pairs[i].split(':');
+    key = split.shift().trim();
+    value = split.join(':').trim();
+    parsedHeaders[key] = value;
+  }
+
+  return parsedHeaders;
+}
+
 test('xhr.getAllResponseHeaders() sends empty string when no headers', function(t) {
   var xhr = new XMLHttpRequest();
   t.equal('', xhr.getAllResponseHeaders(), 'we get an empty string');
@@ -14,6 +32,6 @@ test('xhr.getAllResponseHeaders() sends all response headers when present', func
   xhr.open('GET', '/');
   xhr.send();
   xhr.respond(200, headers);
-  t.deepEqual(xhr.getAllResponseHeaders(), headers, 'we get all the headers');
+  t.deepEqual(parseHeaders(xhr.getAllResponseHeaders()), headers, 'we get all the headers');
   t.end();
 });
